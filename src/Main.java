34c6baf91cae56463533fc86c3f218ae4f06a3d6
Constantine-Medved <medved.k@outlook.com>
2023-03-11 16:46:18 +0300

    Tasks 1 - 3 completed

--- src/Main.java
+++ src/Main.java
@@ -12,14 +12,42 @@ public class Main {
 
     public static void task1() {
         System.out.println("Задача 1.");
+        int monthSavings = 15000;
+        int totalSavings = 0;
+        int monthNo = 0;
+        while (totalSavings < 2_459_000) {
+            monthNo = monthNo + 1;
+            totalSavings = totalSavings + monthSavings;
+            System.out.println("Месяц " + monthNo + ", сумма накоплений равна " + totalSavings + " рублей.");
+        }
+        System.out.println("Чтобы накопить 2459000 рублей, потребуется " + monthNo + " месяцев.");
     }
 
     public static void task2() {
         System.out.println("Задача 2.");
+        int i = 0;
+        while (i <10) {
+            i++;
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (;i > 0; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
     }
 
     public static void task3() {
         System.out.println("Задача 3.");
+        int population = 12_000_000;
+        int annualBirthsPer1000 = 17;
+        int annualDeathsPer1000 = 8;
+        int year = 1;
+        System.out.println("На начало года " + year + ", численность населения составляет " + population + ".");
+        for (;year <=10;year++) {
+            population = population + (population/1000)*(annualBirthsPer1000 - annualDeathsPer1000);
+            System.out.println("Год " + year + ", численность населения составляет " + population + ".");
+        }
     }
 
     public static void task4() {
